60835be3e55e1438ce91502c3a75d808228acb85
honx sinatra <josephhongoa2020@gmail.com>
2024-03-19 01:04:31 +0300

    Developing the smart contract

--- contracts/basicNFT.sol
+++ contracts/basicNFT.sol
@@ -5,12 +5,22 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract basicNFT is ERC721 {
-    string public constant TOKEN_URI =
+    string public constant TOKEN_URI1 =
         "https://ipfs.io/ipfs/QmXsGnz5MA7dsES2HLaNFxzfaKb4hnvX96LZvKaxSShDDB?filename=unyama.png";
+    string public constant TOKEN_URI2 = "";
+    string public constant TOKEN_URI3 = "";
     uint256 private s_tokenCounter;
+    uint256 private random_Number;
+
+    //Array of NFTs
+    string[3] sinatraNFT = [TOKEN_URI1, TOKEN_URI2, TOKEN_URI3];
 
     constructor() ERC721("Sinatra NFT", "SN") {
         s_tokenCounter = 0;
+    }
+
+    function request_Honx_NFT() public returns (string memory) {
+        return sinatraNFT[random_Number];
     }
 
     function mintNft() public returns (uint256) {
